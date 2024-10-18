0ed5685efce1c41f339b8fa7397c2632185c1431
yilbat <mudnoethankgod@gmail.com>
2024-10-18 20:18:38 +0100

    static

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -128,8 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, "static"),
-'moviereviews/static/',
+os.path.join('/home/yilbat/Desktop/Django/Turtorial_Projects/moviereviews/moviereviews/', "static"),
+
 ]
 
 LOGIN_URL = 'loginaccount'
